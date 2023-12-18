6b557241e78419cd9c89e920cae768fe0e1acd5f
andregozali <elizgozali@gmail.com>
2023-12-18 16:21:03 +0700

    api php

---
+++ api/add.php
@@ -0,0 +1,24 @@
+<?php
+  require_once('inc/open_connection.php');
+  $title =$_POST['title'];
+  $content =$_POST['content'];
+  $date = $_POST['date'];
+
+  $query = "INSERT INTO notes (title,content,date)
+            VALUES('$title','$content','$date')";
+  
+  $query_run = mysqli_query($CON,$query);
+
+  if($query_run){
+    echo json_encode([
+      "error" => false,
+      "message" => 'Notes has been successfully added!'
+    ]);
+  }else{echo json_encode([
+    "error" => true,
+    "message" => 'Notes failed to be added!'
+  ]);
+    
+  }
+  require_once('inc/close_connection.php')
+  ?>
---
+++ api/prak_mb.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 18, 2023 at 10:05 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `prak_mb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
+--
+
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `date` date NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `notes`
+--
+
+INSERT INTO `notes` (`id`, `title`, `content`, `date`, `status`) VALUES
+(1, 'Go Shopping', 'List of Products\r\n1. Watermelon\r\n2. Salmon \r\n3. 300ml Milk\r\n', '2023-12-14', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
